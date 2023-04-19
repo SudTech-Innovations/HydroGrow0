2eaf2e17e3b2bce0976b23a90f6a0c3c1a25a715
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-19 16:54:57 +0200

    Update Environnement & plante .java

--- HydroGrow/src/application/Environnement.java
+++ HydroGrow/src/application/Environnement.java
@@ -1,16 +1,30 @@
 package application;
 
 public class Environnement {
+    private double temperature;
+    private double humidite;
 
     public Environnement(double temperature, double humidite) {
+        this.temperature = temperature;
+        this.humidite = humidite;
     }
 
-    public String getTemperature() {
-        return null;
+    public double getTemperature() {
+        return temperature;
     }
 
-    public String getHumidite() {
-        return null;
+    public double getHumidite() {
+        return humidite;
     }
 
+    public boolean isOptimal(Plante plante) {
+        // Comparaison des valeurs d'humidité et de température avec celles recommandées
+        // pour la plante
+        if (this.humidite >= plante.getHumiditeRecommandee()
+                && this.temperature >= plante.getTemperatureRecommandee()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- HydroGrow/src/application/Plante.java
+++ HydroGrow/src/application/Plante.java
@@ -1,20 +1,47 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Plante {
+    private String nom;
+    private double humiditeRecommandee;
+    private double temperatureRecommandee;
 
     public Plante(String nomPlante) {
+        this.nom = nomPlante;
+        // Récupération des valeurs d'humidité et de température recommandées à partir
+        // du fichier CSV
+        String csvFile = "data/plantes.csv";
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+                String[] plante = line.split(cvsSplitBy);
+                if (plante[0].equals(nomPlante)) {
+                    humiditeRecommandee = Double.parseDouble(plante[1]);
+                    temperatureRecommandee = Double.parseDouble(plante[2]);
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getNom() {
-        return null;
+        return nom;
     }
 
-    public String getHumiditeRecommandee() {
-        return null;
+    public double getHumiditeRecommandee() {
+        return humiditeRecommandee;
     }
 
-    public String getTemperatureRecommandee() {
-        return null;
+    public double getTemperatureRecommandee() {
+        return temperatureRecommandee;
     }
-
 }
