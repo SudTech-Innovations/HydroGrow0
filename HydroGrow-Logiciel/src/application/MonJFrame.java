b8576691855551bfedcf93aad79832624adf6e7e
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-04-21 11:58:33 +0200

    Update MonJFrame.java

--- HydroGrow-Logiciel/src/application/MonJFrame.java
+++ HydroGrow-Logiciel/src/application/MonJFrame.java
@@ -2,8 +2,8 @@ package application;
 
 import javax.swing.*;
 import java.awt.event.*;
-import application.Plante;
-import application.Environnement;
+// import application.Plante;
+// import application.Environnement;
 
 
 public class MonJFrame extends JFrame {
@@ -32,38 +32,31 @@ public class MonJFrame extends JFrame {
 		// JLabel pour la sélection de plante
 		JLabel labelPlante = new JLabel("Plante : ");
 		labelPlante.setBounds(20, 10, 70, 50); // Définissez les coordonnées et la taille du label
-		add(labelPlante); // Ajoutez le label au JFrame
 
 		// Création d'une combobox pour la sélection de plante
 		String[] plantes = { "Tomates", "Carottes", "Salade", "Radis", "Poivrons" };
 		JComboBox<String> comboBox = new JComboBox<>(plantes);
 		comboBox.setBounds(120, 20, 150, 30);
-		add(comboBox);
 
 		// JLabel pour la température
 		JLabel labelTemp = new JLabel("Température : ");
 		labelTemp.setBounds(20, 70, 100, 30);
-		add(labelTemp);
 
 		// JTextField pour la température
 		JTextField tempField = new JTextField();
 		tempField.setBounds(120, 70, 150, 30);
-		add(tempField);
 
 		// JLabel pour l'humidité
 		JLabel labelHum = new JLabel("Humidité : ");
 		labelHum.setBounds(20, 120, 100, 30);
-		add(labelHum);
 
 		// JTextField pour l'humidité
 		JTextField humField = new JTextField();
 		humField.setBounds(120, 120, 150, 30);
-		add(humField);
 
 		// JButton pour reset
 		JButton resetButton = new JButton("Reset");
 		resetButton.setBounds(20, 170, 100, 30);
-		add(resetButton);
 
 		resetButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -77,12 +70,10 @@ public class MonJFrame extends JFrame {
 		// JButton pour valider
 		JButton validButton = new JButton("Valider");
 		validButton.setBounds(170, 170, 100, 30);
-		add(validButton);
 
 		// JLabel pour l'affichage de sortie
 		JLabel labelOutput = new JLabel(" ");
 		labelOutput.setBounds(20, 220, 250, 30);
-		add(labelOutput);
 
 		// Action pour le bouton Valider
 		validButton.addActionListener(new ActionListener() {
@@ -105,6 +96,18 @@ public class MonJFrame extends JFrame {
 				}
 			}
 		});
+
+
+		// Ajout des éléments dans le JFrame
+		add(labelPlante); 
+		add(comboBox);
+		add(labelTemp);
+		add(tempField);
+		add(labelHum);
+		add(humField);
+		add(resetButton);
+		add(validButton);
+		add(labelOutput);
 
 		// Affichage de la fenêtre
 		setVisible(true);
