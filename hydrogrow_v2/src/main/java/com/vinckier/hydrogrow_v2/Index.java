e5d779473281872269907af11faf08d4397eeed8
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-05-05 22:33:23 +0200

    Update Index.java
    Ajout de la javadoc

--- hydrogrow_v2/src/main/java/com/vinckier/hydrogrow_v2/Index.java
+++ hydrogrow_v2/src/main/java/com/vinckier/hydrogrow_v2/Index.java
@@ -1,7 +1,16 @@
 package com.vinckier.hydrogrow_v2;
 
+/**
+ * Classe principale du programme HydroGrow_v2 qui lance l'interface
+ * utilisateur.
+ * Si c'est la première fois que le logiciel est installé, il est nécessaire de
+ * créer la base de données "hydrobase"
+ * dans le gestionnaire de base de données (type phpmyadmin).
+ */
 public class Index {
-
+	/**
+	 * Execution du programme
+	 */
 	public static void main(String[] args) {
 		Interface start = new Interface();
 		System.out.println(start);
