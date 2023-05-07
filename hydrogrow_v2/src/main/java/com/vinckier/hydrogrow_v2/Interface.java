586a64df96e22a2fab6bce16a8c209739b1eea72
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-05-07 21:45:28 +0200

    Update Interface.java
    ComboBox fonctionnel

--- hydrogrow_v2/src/main/java/com/vinckier/hydrogrow_v2/Interface.java
+++ hydrogrow_v2/src/main/java/com/vinckier/hydrogrow_v2/Interface.java
@@ -1,89 +1,44 @@
 package com.vinckier.hydrogrow_v2;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.sql.Connection;
-import javax.swing.JButton;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-/**
- * La classe Interface représente l'interface graphique de l'application
- * HydroGrow.
- * Elle permet à l'utilisateur de se connecter à la base de données et
- * d'afficher un message de bienvenue.
- * Elle étend la classe JFrame pour créer une fenêtre.
- */
 public class Interface extends JFrame {
 
-    private JPanel buttonPanel;
-    private JButton btnConnexion;
+    private JPanel contentPane;
+    private JComboBox<String> plantesComboBox;
 
-    /**
-     * Constructeur de la classe Interface.
-     * Initialise la fenêtre avec un titre et une taille prédéfinie, puis appelle la
-     * méthode init pour initialiser les composants.
-     */
     public Interface() {
-        // Titre de la fenêtre
         super("HydroGrow Interface");
-
-        // Taille de la fenêtre
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 400);
 
-        // Action lors de la fermeture de la fenêtre
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Initialisation des composants
         init();
-
-        // Affichage de la fenêtre
         setVisible(true);
     }
 
-    /**
-     * Initialise les composants de l'interface graphique, tels que le bouton de
-     * connexion.
-     * Ajoute également des gestionnaires d'événements pour détecter les clics sur
-     * le bouton.
-     */
     private void init() {
-        // Panneau pour le bouton de connexion
-        buttonPanel = new JPanel();
-        getContentPane().add(buttonPanel, BorderLayout.NORTH);
+        contentPane = new JPanel();
+        getContentPane().add(contentPane, BorderLayout.CENTER);
 
-        // Bouton de connexion
-        btnConnexion = new JButton("Se connecter à la base de données");
-        btnConnexion.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Connexion à la base de données
-                Connection conn = DatabaseManager.getConnection();
-                if (conn == null) {
-                    JOptionPane.showMessageDialog(Interface.this, "La connexion à la base de données a échoué.",
-                            "Erreur", JOptionPane.ERROR_MESSAGE);
-                } else {
-                    JOptionPane.showMessageDialog(Interface.this, "La connexion à la base de données est établie.",
-                            "Succès", JOptionPane.INFORMATION_MESSAGE);
+        plantesComboBox = new JComboBox<String>();
+        contentPane.add(plantesComboBox);
 
-                    // Supprimer le bouton de connexion
-                    buttonPanel.remove(btnConnexion);
-
-                    // Ajouter un label pour afficher "Hello World"
-                    JLabel helloLabel = new JLabel("Hello World !");
-                    getContentPane().add(helloLabel, BorderLayout.CENTER);
-
-                    // Mettre à jour l'affichage de la fenêtre
-                    revalidate();
-                    repaint();
-                }
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String query = "SELECT nom FROM plante";
+            PreparedStatement statement = conn.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                plantesComboBox.addItem(resultSet.getString("nom"));
             }
-        });
-        buttonPanel.add(btnConnexion);
-
-        // Affichage de la fenêtre
-        setVisible(true);
+        } catch (SQLException e) {
+            System.err.println("Erreur lors de la récupération des plantes : " + e.getMessage());
+        }
     }
 }
