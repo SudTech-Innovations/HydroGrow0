9b0cc84a336bf62e7d153d6c5b2640d6673a2991
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-05-10 12:07:37 +0200

    Merge

--- HydroM/src/main/java/bdd/HydroBaseManager.java
+++ HydroM/src/main/java/bdd/HydroBaseManager.java
@@ -1,47 +1,47 @@
-package bdd;
+// package bdd;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+// import java.sql.Connection;
+// import java.sql.DriverManager;
+// import java.sql.SQLException;
+// import java.sql.Statement;
 
-import bdd.models.Plante;
+// import bdd.models.Plante;
 
-/**
- * Si la création de la base de donnée ne marche pas
- * allez dans votre phpmyadmin - SQL
- * Executé la commande suivante :
- * CREATE DATABASE hydrobase;
- */
-public class HydroBaseManager {
+// /**
+//  * Si la création de la base de donnée ne marche pas
+//  * allez dans votre phpmyadmin - SQL
+//  * Executé la commande suivante :
+//  * CREATE DATABASE hydrobase;
+//  */
+// public class HydroBaseManager {
 
-    public static Connection connection;
-    public Statement statement;
-    public String databaseName = "hydrobase";
-    public String username = "root";
-    public String password = "";
-    public String url = "jdbc:mysql://localhost/" + databaseName;
+//     public static Connection connection;
+//     public Statement statement;
+//     public String databaseName = "hydrobase";
+//     public String username = "root";
+//     public String password = "";
+//     public String url = "jdbc:mysql://localhost/" + databaseName;
 
-    public HydroBaseManager() throws SQLException {
-        connection = DriverManager.getConnection(url, username, password);
-        System.out.println("Connexion à la base de données réussie.");
-        // ------------------------------------------------------------- //
+//     public HydroBaseManager() throws SQLException {
+//         connection = DriverManager.getConnection(url, username, password);
+//         System.out.println("Connexion à la base de données réussie.");
+//         // ------------------------------------------------------------- //
 
-        HydroTablePlante HydroTable = new HydroTablePlante(connection);
-        System.out.println(HydroTable);
-    }
+//         HydroTablePlante HydroTable = new HydroTablePlante(connection);
+//         System.out.println(HydroTable);
+//     }
 
-    public void close() throws SQLException {
-        connection.close();
-        System.out.println("Connexion à la base de données fermée.");
-    }
+//     public void close() throws SQLException {
+//         connection.close();
+//         System.out.println("Connexion à la base de données fermée.");
+//     }
 
-    public static Connection getConnection() {
-        return connection;
-    }
+//     public static Connection getConnection() {
+//         return connection;
+//     }
 
-    public void createLegume(String nom, String temperature, String humidite){
-        Plante newplante = new Plante(nom, temperature, humidite);
-    }
+//     public void createLegume(String nom, String temperature, String humidite){
+//         Plante newplante = new Plante(nom, temperature, humidite);
+//     }
 
-}
+// }
--- HydroM/src/main/java/executable/Index.java
+++ HydroM/src/main/java/executable/Index.java
@@ -1,17 +1,17 @@
-package executable;
+// package executable;
 
-import java.io.IOException;
-import java.sql.SQLException;
+// import java.io.IOException;
+// import java.sql.SQLException;
 
-import bdd.*;
+// import bdd.*;
 
-public class Index {
+// public class Index {
 
-	public static void main(String[] args) throws IOException, SQLException {
-		HydroBaseManager HydroBase = new HydroBaseManager();
-		System.out.println(HydroBase);
+// 	public static void main(String[] args) throws IOException, SQLException {
+// 		HydroBaseManager HydroBase = new HydroBaseManager();
+// 		System.out.println(HydroBase);
 
 		
-	}
+// 	}
 
-}
+// }
