6e32e6cc339e0a8049628d4506135761774639d0
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-05-12 19:33:39 +0200

    Premier test
    Utiliser l'API trefle

--- hydrogrow_v3/src/main/java/vinckier/hydrogrow_v3/Index.java
+++ hydrogrow_v3/src/main/java/vinckier/hydrogrow_v3/Index.java
@@ -1,9 +1,21 @@
 package vinckier.hydrogrow_v3;
 
-public class Index 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.web.client.RestTemplate;
+
+public class Index {
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+        String apiUrl = "https://trefle.io/api/v1/plants?token={token}&filter_not[common_name]=null&order[common_name]=asc&page_size=10";
+
+        // Remplacez {token} par votre clé API Trefle
+        String apiKey = "7e9WnfXHyYnks7ZQpHdmmyplCR1TawDWb7iXOl04pZo";
+        String url = apiUrl.replace("{token}", apiKey);
+
+        // Effectue une requête GET sur l'API Trefle en utilisant RestTemplate
+        TrefleResponse response = restTemplate.getForObject(url, TrefleResponse.class);
+
+        // Extraire les résultats
+        System.out.println("Liste des 10 premiers éléments de l'API Trefle :");
+        response.getData().stream().forEach(item -> System.out.println(item.getScientific_name()));
     }
 }
---
+++ hydrogrow_v3/src/main/java/vinckier/hydrogrow_v3/TrefleResponse.java
@@ -0,0 +1,15 @@
+package vinckier.hydrogrow_v3;
+
+import java.util.List;
+
+public class TrefleResponse {
+    private List<TreflePlant> data;
+
+    public List<TreflePlant> getData() {
+        return data;
+    }
+
+    public void setData(List<TreflePlant> data) {
+        this.data = data;
+    }
+}
