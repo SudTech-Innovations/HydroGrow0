77f8d7131088327b551f2d558da5e89b30f4ac4a
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-05-12 17:42:50 +0200

    HydroGrow V3

---
+++ hydrogrow_v3/src/main/java/vinckier/hydrogrow_v3/App.java
@@ -0,0 +1,13 @@
+package vinckier.hydrogrow_v3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
