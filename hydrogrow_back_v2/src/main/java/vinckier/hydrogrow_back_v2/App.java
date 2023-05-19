12f6197435edc9ac8370186de1a9b02832cc7a92
Nicolas VINCKIER <nicolas.vinckier05@gmail.com>
2023-05-19 22:10:35 +0200

    Front + back
    Ajout de back v2 pour le nouveau développement du back end
    Changement du front, ajout d'une page de bienvenue

---
+++ hydrogrow_back_v2/src/main/java/vinckier/hydrogrow_back_v2/App.java
@@ -0,0 +1,13 @@
+package vinckier.hydrogrow_back_v2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
